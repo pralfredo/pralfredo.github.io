704830b28601dd3d6d178a127b4a80c554aea5db
pralfredo <132622261+pralfredo@users.noreply.github.com>
2025-10-24 17:28:15 -0400

    CORS issue fixed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.161.0";
+import * as THREE from "https://cdn.skypack.dev/three@0.161.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.161.0/examples/jsm/controls/OrbitControls.js";
 
 /* ---------- Scene, Camera, Renderer ---------- */
